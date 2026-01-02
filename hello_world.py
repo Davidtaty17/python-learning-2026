911a983a674a2a061d0ce28f0f8f1c612a8beed8
Davidtaty17 <davidjldt@gmail.com>
2026-01-01 23:27:14 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# hello_world.py
+# It's my first python script. I'm excited to start with code :) -January 2026
+#Author: David Taty
+# My Goal: Being a cloud security engineer by 2030 (big challenge but i can do it )
+
