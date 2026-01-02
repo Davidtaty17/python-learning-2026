b1da342aa5ded4f4cf44a41fa87c080cd3447e6e
wh1t3roz3 <davidjldt@gmail.com>
2026-01-02 08:17:04 -0500

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,52 @@
 #Author: David Taty
 # My Goal: Being a cloud security engineer by 2030 (big challenge but i can do it )
 
+#  Variables basics 
+first_name='David' # string variable
+last_name='Taty' # string variable
+full_name= first_name + last_name
+country='Canada' # string variable
+city= 'Toronto' # string variable
+age= 22 # integer variable
+is_student= True # boolean variable
+is_studying_what='computer science' # string variable
+
+
+
+#Print everything 
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(city)
+print(age)
+print(is_student)
+print(is_studying_what)
+
+
+
+# checking data types 
+print((10))   #quel type? integer (nombre entier)
+print(type(3.14)) # float (nombre décimal)
+print(type("Hello")) # string (chaîne de caractères)
+print(type(True)) # boolean (valeur booléenne)
+print(type(3+2)) # integer (nombre entier)
+print(type(3/2)) # float (nombre décimal)
+print(type(10%3)) # integer (nombre entier)
+print(type(10//3)) # integer (nombre entier)
+print(type(3**2)) # integer (nombre entier
+print(type("3"+ "2")) # string (chaîne de caractères
+print(type({'name': 'David'})) # dictionary (dictionnaire)
+print(type((9.81, 2.71))) # tuple (tuple)
+print(({1, 2, 3})) # set (ensemble)
+
+
+#types 
+print(type(first_name)) # string variable
+print(type(last_name)) # string variable
+print(type(full_name)) # string variable
+print(type(country)) # string variable
+print(type(city)) # string variable
+print(type(age)) # integer variable
+print(type(is_student)) # boolean variable
+print(type(is_studying_what)) # string variable
